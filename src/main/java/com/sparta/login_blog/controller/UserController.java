6712be4cb62c8746854ae1d2d020188a9e76a14a
SeungBum Baek <wer06099@naver.com>
2023-06-23 17:13:55 +0900

    User 경로 리팩토링

--- src/main/java/com/sparta/login_blog/controller/UserController.java
+++ src/main/java/com/sparta/login_blog/controller/UserController.java
@@ -8,7 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/auth")
 public class UserController {
     private final UserService userService;
 
