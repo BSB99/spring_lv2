f88f813f109752d4a09b01060208c9165e542288
SeungBum Baek <wer06099@naver.com>
2023-07-13 11:14:05 +0900

    Param 명 리팩터링

--- src/main/java/com/sparta/login_blog/controller/PostController.java
+++ src/main/java/com/sparta/login_blog/controller/PostController.java
@@ -1,8 +1,6 @@
 package com.sparta.login_blog.controller;
 
 import com.sparta.login_blog.dto.*;
-import com.sparta.login_blog.entity.User;
-import com.sparta.login_blog.jwt.JwtUtil;
 import com.sparta.login_blog.security.UserDetailsImpl;
 import com.sparta.login_blog.service.PostService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,7 +16,6 @@ public class PostController {
 
     private final PostService postService;
 
-    private final JwtUtil jwtUtil;
     @PostMapping("post")
     @Operation(summary = "게시글 생성", description = "게시글 생성 API")
     public ResponseEntity<PostResponseDto> createPost(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody PostRequestDto requestDto) {
@@ -36,26 +33,26 @@ public class PostController {
         return ResponseEntity.status(200).body(result);
     }
 
-    @GetMapping("/post/{id}")
+    @GetMapping("/post/{postNo}")
     @Operation(summary = "게시글 상세 조회", description = "게시글 상세 조회 API")
-    public ResponseEntity<PostDetailResponseDto> getPostById(@PathVariable Long id) {
-        PostDetailResponseDto result = postService.getPostById(id);
+    public ResponseEntity<PostDetailResponseDto> getPostById(@PathVariable Long postNo) {
+        PostDetailResponseDto result = postService.getPostById(postNo);
 
         return ResponseEntity.status(200).body(result);
     }
 
-    @PutMapping("/post/{id}")
+    @PutMapping("/post/{postNo}")
     @Operation(summary = "게시글 수정", description = "게시글 수정 API")
-    public ResponseEntity<PostResponseDto> updatePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id, @RequestBody PostRequestDto requestDto) {
-        PostResponseDto result = postService.updatePost(id, requestDto, userDetails.getUser());
+    public ResponseEntity<PostResponseDto> updatePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postNo, @RequestBody PostRequestDto requestDto) {
+        PostResponseDto result = postService.updatePost(postNo, requestDto, userDetails.getUser());
 
         return ResponseEntity.status(200).body(result);
     }
 
-    @DeleteMapping("/post/{id}")
+    @DeleteMapping("/post/{postNo}")
     @Operation(summary = "게시글 삭제", description = "게시글 삭제 API")
-    public ResponseEntity<ApiResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long id) {
-        ApiResponseDto result = postService.deletePost(id, userDetails.getUser());
+    public ResponseEntity<ApiResponseDto> deletePost(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postNo) {
+        ApiResponseDto result = postService.deletePost(postNo, userDetails.getUser());
 
         return ResponseEntity.status(200).body(result);
     }
